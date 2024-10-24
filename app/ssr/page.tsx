70da90c5aaef8f598a83d9516841ffceb4c42755
Sahil <shaikhsahil4883@gmail.com>
2024-10-24 21:16:40 +0530

    Making the setup properly

--- app/ssr/page.tsx
+++ app/ssr/page.tsx
@@ -1,12 +1,12 @@
-import { createClerkSupabaseClientSsr } from './client';
-import AddTaskForm from './AddTaskForm';
+import { createClerkSupabaseClientSsr } from "./client";
+import AddTaskForm from "./AddTaskForm";
 
 export default async function Home() {
   // Use the custom Supabase client you created
   const client = createClerkSupabaseClientSsr();
 
   // Query the 'tasks' table to render the list of tasks
-  const { data, error } = await client.from('tasks').select();
+  const { data, error } = await client.from("tasks").select();
   if (error) {
     throw error;
   }
--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,5 @@
 import { clerkMiddleware } from "@clerk/nextjs/server";
 
-// This Middleware does not protect any routes by default.
-// See https://clerk.com/docs/references/nextjs/clerk-middleware for more information about configuring your Middleware
 export default clerkMiddleware();
 
 export const config = {
